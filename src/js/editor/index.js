c58c62a1b8678e92080f8586fd9e227509427157
wanglei <wanglei04@kanzhun.com>
2019-08-14 14:59:13 +0800

    取消光标自动聚焦

--- src/js/editor/index.js
+++ src/js/editor/index.js
@@ -235,7 +235,7 @@ Editor.prototype = {
         }
 
         this.selection.createRangeByElem($last, false, true)
-        this.selection.restoreSelection()
+        // this.selection.restoreSelection()
     },
 
     // 绑定事件
